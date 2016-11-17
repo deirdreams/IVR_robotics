d745a17a8a3c83cc88504c53d65095965bfc97dc
tomcek112 <thomas.lutzeyer@gmail.com>
2016-11-17 11:42:32 +0000

    avoider pid test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ev3dev.ev3 as ev3
 from time import sleep
+from pid import PidController
 
 class Pablo:
 
@@ -50,8 +51,8 @@ class Pablo:
                 self.leftMotor.reset()
                 return self
 
-        def __runMotor(self, motor):
-                motor.run_direct(duty_cycle_sp=30)
+        def __runMotor(self, motor, sp):
+                motor.run_direct(duty_cycle_sp=sp)
                 return self
 
         def __turn90(self, direction):
@@ -59,12 +60,12 @@ class Pablo:
                 self.__resetMotors()
 
                 if direction == 'r':
-                        self.__runMotor(self.leftMotor)
+                        self.__runMotor(self.leftMotor, 30)
                         sleep(2.2)
                         self.__resetMotors()
 
                 elif direction == 'l':
-                        self.__runMotor(self.leftMotor)
+                        self.__runMotor(self.leftMotor, 30)
                         sleep(2.2)
                         self.__resetMotors()
 
@@ -97,11 +98,30 @@ class Pablo:
                 sleep(1)
                 return detected
 
+        def __turnHead90(self, direction):
+        	if direction == 'r':
+        		self.head.run_to_abs_pos(duty_cycle_sp=30, position_sp=90)
+        	elif direction == 'l':
+        		self.head.run_to_abs_pos(duty_cycle_sp=30, position_sp=-90)
+        	else:
+                raise RuntimeError('Unknown direction')
+
+        def __avoidObject(self, snapshot):
+        	self.__resetMotors()
+        	pid = PidController(snapshot, 1, 0, 0)
+        	while (self.color.value() > 60):
+        		self.__runMotor(self.leftMotor, 10)
+        		self.__runMotor(self.rightMotor, pid.getPower(self.sonar.value()))
+
         def run(self):
                 detected = 0
                 #while(not detected):
                 self.__follow_line(0)
                 #       detected = self.__findObject()
                 self.__resetMotors()
+                val = self.sonar.value()
                 self.__turn90('r')
-                self.__findObject()
+                self.__turnHead90('l')
+                #self.__findObject()
+                self.__avoidObject(val)
+                self.follow_line(0)
